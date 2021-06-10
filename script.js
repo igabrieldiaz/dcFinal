e8ffee001acdafbfc9a95562aebfb99d3180f8a3
igabrieldiaz <72045080+igabrieldiaz@users.noreply.github.com>
2021-06-10 01:59:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+  $(function() {
+    $( "#lincoln" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#gathering" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#march" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#warmemorial" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#mlk" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#pool" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#rosaparks" ).draggable({stack: 'div'});
+
+  });
+  
+  $(function() {
+	  $( "#voting" ).draggable({stack: 'div'});
+
+  });
+  
+  $(function() {
+	  $( "#votingmarch" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#blm" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#foggy" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#smithsonian" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#africanAmericanMuseum" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#georgetown" ).draggable({stack: 'div'});
+
+  });
+
+  $(function() {
+	  $( "#fake" ).draggable({stack: 'div'});
+
+  });
+
+//   $('#gathering').mouseenter(function(){
+//       $('#gathering .text-position p').css('display', 'block')
+//   })
+//   $('#gathering').mouseleave(function(){
+//       $('#gathering .text-position p').css('display', 'none')
+//   })
